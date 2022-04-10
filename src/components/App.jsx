cebf1c4f5a4cec3be42533c2fb3af9d24c6c0521
Yyyurii <yurii.kryvko@gmail.com>
2022-04-11 02:53:15 +0300

    Верстка секції post-method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,12 +35,11 @@ function App() {
       </section>
 
       <section className='get-method _container'>
-
         <span className='heading'>Working with GET request</span>
 
         <div className='get-method__content'>
 
-          <div className='user-card'>
+          <article className='user-card'>
             <div className='user-card__photo'>
               <img src={userPhoto} alt="user photo" />
             </div>
@@ -56,8 +55,8 @@ function App() {
             <div className='user-card__phone'>
               <span>+38 (098) 278 44 24</span>
             </div>
-          </div>
-          <div className='user-card'>
+          </article>
+          <article className='user-card'>
             <div className='user-card__photo'>
               <img src={userPhoto} alt="user photo" />
             </div>
@@ -73,8 +72,8 @@ function App() {
             <div className='user-card__phone'>
               <span>+38 (098) 278 44 24</span>
             </div>
-          </div>
-          <div className='user-card'>
+          </article>
+          <article className='user-card'>
             <div className='user-card__photo'>
               <img src={userPhoto} alt="user photo" />
             </div>
@@ -90,8 +89,8 @@ function App() {
             <div className='user-card__phone'>
               <span>+38 (098) 278 44 24</span>
             </div>
-          </div>
-          <div className='user-card'>
+          </article>
+          <article className='user-card'>
             <div className='user-card__photo'>
               <img src={userPhoto} alt="user photo" />
             </div>
@@ -107,8 +106,8 @@ function App() {
             <div className='user-card__phone'>
               <span>+38 (098) 278 44 24</span>
             </div>
-          </div>
-          <div className='user-card'>
+          </article>
+          <article className='user-card'>
             <div className='user-card__photo'>
               <img src={userPhoto} alt="user photo" />
             </div>
@@ -124,8 +123,8 @@ function App() {
             <div className='user-card__phone'>
               <span>+38 (098) 278 44 24</span>
             </div>
-          </div>
-          <div className='user-card'>
+          </article>
+          <article className='user-card'>
             <div className='user-card__photo'>
               <img src={userPhoto} alt="user photo" />
             </div>
@@ -141,10 +140,57 @@ function App() {
             <div className='user-card__phone'>
               <span>+38 (098) 278 44 24</span>
             </div>
-          </div>
+          </article>
 
         </div>
         <button className='button button_show-more'>Show more</button>
+
+      </section>
+
+      <section className='post-method _container'>
+        <span className='heading'>Working with POST request</span>
+
+        <div className='post-method__form'>
+          <form className='form'>
+
+            <input className='form__input-info' name='name' placeholder='Your name' />
+            <input className='form__input-info' name='email' placeholder='Email' />
+            <input className='form__input-info' name='phone' placeholder='Phone' />
+
+            <div className='form__checkbox-container'>
+              <p>Select your position</p>
+
+              <div>
+                <input type="checkbox" id="frontend" name="frontend" defaultChecked />
+                <label htmlFor="frontend">Frontend developer</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="backend" name="backend" />
+                <label htmlFor="backend">Backend developer</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="designer" name="designer" />
+                <label htmlFor="designer">Designer</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="qa" name="qa" />
+                <label htmlFor="designer">QA</label>
+              </div>
+            </div>
+
+            <div className='form__upload'>
+              <button className=''>Upload</button>
+              <input className='' name='upload' placeholder='Upload ypur photo' />
+            </div>
+
+            <button className='form__btn-singUp button button_disabled'>Sing up</button>
+
+          </form>
+        </div>
+
       </section>
 
     </div>
