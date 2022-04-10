38ac75e98e3d6217b02fe6b0c5cb2a0f264b4fea
Yyyurii <yurii.kryvko@gmail.com>
2022-04-10 21:54:36 +0300

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,30 @@
 import './App.scss';
+import logo from '../assets/images/icons/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <div className="header__div">
-          <h1>Check font family</h1>
+    <div className='App'>
+
+      <header className='header _container'>
+        <div className='header__container'>
+
+          <div className='logo'>
+            <div className='logo__img'>
+              <img src={logo} alt='logo' />
+            </div>
+            <div className='logo__label'>
+              <span>TESTTASK</span>
+            </div>
+          </div>
+
+          <div className='header__btns-container'>
+            <button className='button'>Users</button>
+            <button className='button'>Sing up</button>
+          </div>
+
         </div>
       </header>
+
     </div>
   );
 }
