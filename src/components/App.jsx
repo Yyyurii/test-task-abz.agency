a93d28cfe764931756f5c189a8322a01759cf102
Yyyurii <yurii.kryvko@gmail.com>
2022-04-10 20:42:43 +0300

    Встановлення sass
    Обнулення базових налаштувань, створення основних змінних scss, підключення шрифтів

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="header__div">
+          <h1>Check font family</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
