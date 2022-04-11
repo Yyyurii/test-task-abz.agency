e569fd165a2031d92247531a1eaa9b313e5eca07
Yyyurii <yurii.kryvko@gmail.com>
2022-04-11 12:37:31 +0300

    Рефакторинг кода

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,9 +1,9 @@
 import './App.scss';
 
-import Header from './header';
-import MainImg from './mainImg';
-import GetMethod from './getMethod';
-import PostMethod from './postMethod';
+import Header from '../header';
+import MainImg from '../mainImg';
+import GetMethod from '../getMethod';
+import PostMethod from '../postMethod';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './index.scss';
 
-import App from './components/App';
+import App from './components/app';
 
 ReactDOM.render(
   <React.StrictMode>
